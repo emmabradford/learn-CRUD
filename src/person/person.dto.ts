7cec543a6359624dec0ede1c0fbbb63832f911f0
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-28 11:07:33 -0500

    added person dto

---
+++ src/person/person.dto.ts
@@ -0,0 +1,8 @@
+export class PersonDto {
+  readonly id: number;
+  readonly name: string;
+  readonly title: string;
+  readonly partner: string;
+  readonly home: string;
+  readonly weapon: string;
+}
