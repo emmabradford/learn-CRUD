e0d010eb66f9f02fce3c3e7f1504fb2ecd3e3c5f
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-28 10:51:14 -0500

    filled put people service functions

--- src/person/person.mok.ts
+++ src/person/person.mok.ts
@@ -1,6 +1,6 @@
 export const PEOPLE = [
   {
-    id: '1000000',
+    id: 1000000,
     name: 'Wei Ying',
     title: 'Yiling Laozu',
     partner: 'Lan Zhan',
--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,5 +1,5 @@
 import { throwStatement } from '@babel/types';
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { PEOPLE } from './person.mok';
 
 @Injectable()
@@ -11,18 +11,37 @@ export class PersonService {
   }
 
   public async postPerson(person) {
-    return;
+    return this.people.push(person);
   }
 
-  public async getPersonById(id) {
-    return;
+  public async getPersonById(id: number) {
+    const person = this.people.find((person) => person.id === id);
+    if (person) {
+      throw new HttpException('Not Found', 404);
+    }
+    return person;
   }
 
-  public async deletePersonById(id) {
-    return;
+  public async deletePersonById(id: number) {
+    const i = this.people.findIndex((person) => person.id === id);
+    if (i === -1) {
+      throw new HttpException('Not Found', 404);
+    }
+    this.people.splice(i, 1);
+    return this.people;
   }
 
-  public async putPersonByID(id) {
-    return;
+  public async putPersonByID(
+    id: number,
+    propertyName: string,
+    propertValue: string,
+  ) {
+    const i = this.people.findIndex((person) => person.id === id);
+    if (i === -1) {
+      throw new HttpException('Not Found', 404);
+    }
+    this.people[i][propertyName] = propertValue;
+
+    return this.people;
   }
 }
