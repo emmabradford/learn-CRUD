b49e0d905c6a8f8bda960d8da4db4d328d04a2e4
EmmaAbe <emma.abe.bradford@gmail.com>
2021-07-06 16:00:12 -0500

    updated all the person service methods

--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -12,22 +12,43 @@ export class PersonService {
   ) {}
 
   public async getPeople() {
-    const people = this.personModel.find().exec();
+    const people = await this.personModel.find().exec();
     if (!people || !people[0]) {
       throw new HttpException('not found', 404);
     }
-    return cars;
+    return people;
   }
 
-  public async postPerson(person) {}
+  public async postPerson(newPerson) {
+    const person = await this.personModel(newPerson);
+    return person.save();
+  }
 
-  public async getPersonById(id: number): Promise<any> {}
+  public async getPersonById(id: number): Promise<any> {
+    const people = await this.personModel.findOne({ id }).exec();
+    if (!people) {
+      throw new HttpException('not found', 404);
+    }
+    return people;
+  }
 
-  public async deletePersonById(id: number): Promise<any> {}
+  public async deletePersonById(id: number): Promise<any> {
+    const person = await this.personModel.deleteOne({ id }).exec();
+    if (person.deletedCount === 0) {
+      throw new HttpException('Not found', 404);
+    }
+    return person;
+  }
 
   public async putPersonByID(
     id: number,
     propertyName: string,
     propertValue: string,
-  ): Promise<any> {}
+  ): Promise<any> {
+    const person = await this.personModel.findOne({ id }).exec();
+    if (!person) {
+      throw new HttpException('Not Found', 404);
+    }
+    return person;
+  }
 }
